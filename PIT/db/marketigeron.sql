e66a8ba67ef5d87b181749b42ade069b91c54f6e
João Oliveira Félix <124721916+JoaoOFelix@users.noreply.github.com>
2024-10-07 10:56:24 -0300

    Banco de dados atualizado
    -Criado tabela "favoritar"

--- PIT/db/marketigeron.sql
+++ PIT/db/marketigeron.sql
@@ -1,144 +1,108 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: localhost:3306
--- Tempo de geração: 24/06/2024 às 13:34
--- Versão do servidor: 10.5.20-MariaDB
--- Versão do PHP: 7.3.33
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Banco de dados: `id22287464_marketiger`
+-- Banco de dados: `marketiger`
 --
 
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `cadastro`
+-- Estrutura da tabela `cadastro`
 --
 
-CREATE TABLE `cadastro` (
-  `id` int(100) NOT NULL,
+DROP TABLE IF EXISTS `cadastro`;
+CREATE TABLE IF NOT EXISTS `cadastro` (
+  `id` int(100) NOT NULL AUTO_INCREMENT,
   `usuario` varchar(100) NOT NULL,
   `email` varchar(50) NOT NULL,
   `telefone` varchar(18) NOT NULL,
   `senha` varchar(20) NOT NULL,
-  `linkFoto` mediumtext DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `linkFoto` mediumtext,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UNIQUE` (`usuario`)
+) ENGINE=InnoDB;
 
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `comentarios`
+-- Estrutura da tabela `comentarios`
 --
 
-CREATE TABLE `comentarios` (
-  `id` int(100) NOT NULL,
+DROP TABLE IF EXISTS `comentarios`;
+CREATE TABLE IF NOT EXISTS `comentarios` (
+  `id` int(100) NOT NULL AUTO_INCREMENT,
   `secao` varchar(20) NOT NULL,
   `comentario` tinytext NOT NULL,
   `id_item` int(100) NOT NULL,
   `id_comentador` int(100) NOT NULL,
-  `comentador` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `comentador` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_ID_COMENTADOR` (`id_comentador`),
+  KEY `FK_ID_PERFIL` (`id_item`)
+) ENGINE=InnoDB;
 
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `produtos`
+-- Estrutura da tabela `favoritar`
 --
 
-CREATE TABLE `produtos` (
-  `id` int(100) NOT NULL,
+DROP TABLE IF EXISTS `favoritar`;
+CREATE TABLE IF NOT EXISTS `favoritar` (
+  `id` int(100) NOT NULL AUTO_INCREMENT,
+  `id_produto` int(100) NOT NULL,
+  `id_usuario` int(100) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_id_usuario` (`id_usuario`),
+  KEY `fk_id_produto` (`id_produto`)
+) ENGINE=InnoDB;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `produtos`
+--
+
+DROP TABLE IF EXISTS `produtos`;
+CREATE TABLE IF NOT EXISTS `produtos` (
+  `id` int(100) NOT NULL AUTO_INCREMENT,
   `id_usuario` int(255) NOT NULL,
-  `anunciante` varchar(100) DEFAULT NULL,
+  `anunciante` varchar(100)NOT NULL,
   `confiabilidade` int(5) NOT NULL,
   `produto` varchar(80) NOT NULL,
-  `descricao` text DEFAULT NULL,
+  `descricao` text,
   `categoria` varchar(100) DEFAULT NULL,
-  `material` varchar(80) DEFAULT NULL,
-  `tamanho` varchar(20) DEFAULT NULL,
+  `material` varchar(80)DEFAULT NULL,
+  `tamanho` varchar(20)DEFAULT NULL,
   `condicao` varchar(7) DEFAULT NULL,
-  `link-img` longtext DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `preco` decimal(10,2) NOT NULL,
+  `link-img` longtext,
+  PRIMARY KEY (`id`),
+  KEY `fk_id_usuario` (`id_usuario`) USING BTREE,
+  KEY `fk_anunciante` (`anunciante`) USING BTREE
+) ENGINE=InnoDB;
 
 --
--- Índices para tabelas despejadas
---
-
---
--- Índices de tabela `cadastro`
---
-ALTER TABLE `cadastro`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `UNIQUE` (`usuario`);
-
---
--- Índices de tabela `comentarios`
---
-ALTER TABLE `comentarios`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `FK_ID_COMENTADOR` (`id_comentador`),
-  ADD KEY `FK_ID_PERFIL` (`id_item`);
-
---
--- Índices de tabela `produtos`
---
-ALTER TABLE `produtos`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_id_usuario` (`id_usuario`) USING BTREE,
-  ADD KEY `fk_anunciante` (`anunciante`) USING BTREE;
-
---
--- AUTO_INCREMENT para tabelas despejadas
+-- Restrições para despejos de tabelas
 --
 
 --
--- AUTO_INCREMENT de tabela `cadastro`
---
-ALTER TABLE `cadastro`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de tabela `comentarios`
---
-ALTER TABLE `comentarios`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de tabela `produtos`
---
-ALTER TABLE `produtos`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
-
---
--- Restrições para tabelas despejadas
---
-
---
--- Restrições para tabelas `comentarios`
+-- Limitadores para a tabela `comentarios`
 --
 ALTER TABLE `comentarios`
   ADD CONSTRAINT `FK_ID_COMENTADOR` FOREIGN KEY (`id_comentador`) REFERENCES `cadastro` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `FK_ID_PERFIL` FOREIGN KEY (`id_item`) REFERENCES `cadastro` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Restrições para tabelas `produtos`
+-- Limitadores para a tabela `favoritar`
+--
+ALTER TABLE `favoritar`
+  ADD CONSTRAINT `fk_id_produto` FOREIGN KEY (`id_produto`) REFERENCES `produtos` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `fk_id_usuario` FOREIGN KEY (`id_usuario`) REFERENCES `cadastro` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Limitadores para a tabela `produtos`
 --
 ALTER TABLE `produtos`
   ADD CONSTRAINT `fk_anunciante` FOREIGN KEY (`anunciante`) REFERENCES `cadastro` (`usuario`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `fk_usuario` FOREIGN KEY (`id_usuario`) REFERENCES `cadastro` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
